9ab23abe07c6c31519a1fc624c732fb85adbc400
Hunter Hewitt <thehunterhewitt@gmail.com>
2022-10-04 09:37:10 -0600

    variables and logic being built in java

---
+++ blackjack.js
@@ -0,0 +1,14 @@
+let suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
+let values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+
+const newGame = () => {
+
+}
+
+const newDeck = () => {
+
+}
+
+const startGame = () => {
+
+}
